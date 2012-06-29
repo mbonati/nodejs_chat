53cb242cd364c8dd5e43f256b61113abb8841782
Marco Bonati <marco.bonati@gmail.com>
2012-06-29 10:22:58 +0200

    added session timeout congurable

--- src/server/server.js
+++ src/server/server.js
@@ -7,11 +7,12 @@ var app = express.createServer();
 
 var httpPort = cfg.get("http:port");
 var useSSL = cfg.get("http:useSSL");
+var sessionMaxAge = cfg.get("session:maxAge");
 
 //Setup the app
 app.use(connect.logger());
 app.use(express.cookieParser());
-app.use(express.session({ secret: "deepp blue",  svc_id: "nodejs_chat_svc" }));
+app.use(express.session({ secret: "deepp blue",  svc_id: "nodejs_chat_svc", cookie:{maxAge:sessionMaxAge} }));
 
 //Start the server
 app.listen(httpPort);
@@ -20,8 +21,14 @@ console.log("Chat server is listening at port: " + httpPort + " (useSSL="+useSSL
 
 //Routing
 app.get('/', function(req, res){
-	console.log("Session: "+ req.session);
-  res.send('hello world');
+	var session = req.session;
+	if (session.counter){
+		session.counter++;
+	} else {
+		session.counter = 1;
+	}
+	console.log(session.counter +" visits for Session: "+ req.session);
+  res.send('hello world ' + session.counter + ". Expires in "+(session.cookie.maxAge / 1000)+" sec.");
 });
 
 
